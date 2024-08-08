b23ca3442aedb69cadc47431ffef1a6022d7dd9c
Roc Ju <dvs@vip.sina.com>
2024-08-08 15:34:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, AutoModel
 from openxlab.model import download
 
 base_path = './internlm2-chat-7b'
-os.system(f'git clone https://code.openxlab.org.cn/OpenLMLab/internlm2-chat-7b.git {base_path}')
+os.system(f'mkdir ./internlm2-chat-7b  && git clone https://code.openxlab.org.cn/OpenLMLab/internlm2-chat-7b.git {base_path}')
 os.system(f'cd {base_path} && git lfs pull')
 
 tokenizer = AutoTokenizer.from_pretrained(base_path,trust_remote_code=True)
